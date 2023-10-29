9cf1e25002f2c586d25050fd290e4ba44be43733
gojali99 <fajarsidik1999@gmail.com>
2023-10-29 11:54:31 +0700

    added back button

--- app/src/main/java/com/satria/dicoding/latihan/storyapp_submission/view/story_detail/StoryDetailActivity.kt
+++ app/src/main/java/com/satria/dicoding/latihan/storyapp_submission/view/story_detail/StoryDetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.satria.dicoding.latihan.storyapp_submission.R
 import com.satria.dicoding.latihan.storyapp_submission.databinding.ActivityStoryDetailBinding
 import com.satria.dicoding.latihan.storyapp_submission.model.api_response.ListStoryItem
 
@@ -14,6 +15,10 @@ class StoryDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityStoryDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.customAppBar.customAppBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.round_arrow_back_ios_new_24)
 
         val storyData = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra(EXTRA_DATA, ListStoryItem::class.java)
@@ -26,9 +31,11 @@ class StoryDetailActivity : AppCompatActivity() {
         binding.tvUserName.text = storyData?.name
         binding.tvDescription.text = storyData?.description
 
-        binding.btnBack.setOnClickListener {
-            finish()
-        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 
     companion object {
